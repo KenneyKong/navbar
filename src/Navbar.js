e8105102341eaba4ee624a224afd57dfe00f2bef
[Kenney Kong] <[kenneykong@gmail.com]>
2023-06-05 20:55:43 -0400

    created Navbar.js

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div className='nav--container'>
+            <h1 className='dev'>DevAccelerator</h1>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </nav>
+        </div>
+    )
+}
